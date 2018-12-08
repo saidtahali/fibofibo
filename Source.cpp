cc9d16a16fa26e756d51a632992b28c707e31583
Said Tahali <34029975+saidtahali@users.noreply.github.com>
2018-12-08 06:08:06 +0100

    sequence steps start with a 1 not with a 0

--- Source.cpp
+++ Source.cpp
@@ -11,7 +11,7 @@ int main() {
 		b[i] = 0;
 	}
 	b [0] = 1;
-	int x = 0;
+	int x = 1;
 	while (1) {
 		walkThrow(a, b);
 		x++;
@@ -20,11 +20,11 @@ int main() {
 			break;
 		}
 		walkThrow(b, a);
+		x++;
 		if (b[length-1] > 0) {
 			logArray(b);
 			break;
 		}
-		x++;
 	}
 	cout << "F" << x;
 	return 0;
